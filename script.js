ebbc157d61e1e85887220af7c8afe88fabf93c87
Ken Nguyen <46609017+kennn1510@users.noreply.github.com>
2025-01-10 23:22:04 +0700

    Add dialog/modal and update display after submit

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const myLibrary = [];
 const table = document.querySelector("table");
+const newBookButton = document.querySelector("#new-book-btn");
+const form = document.querySelector("form");
+const dialog = document.querySelector("dialog");
+const submit = document.querySelector("input[type='submit']");
 
 function Book(title, author, pages, hasReadBook) {
   this.title = title;
@@ -21,9 +25,41 @@ addBookToLibrary("Angry Joe", "Joe Bobby", 100, false);
 addBookToLibrary("Evil Joe", "Joe Buddy", 1, true);
 addBookToLibrary("Happy Joe", "Bobby Joe", 10, false);
 
-for (book of myLibrary) {
+newBookButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+submit.addEventListener(
+  "click",
+  (event) => {
+    event.preventDefault();
+    console.log(form["title"].value);
+    addBookToLibrary(
+      form["title"].value,
+      form["author"].value,
+      form["pages"].value,
+      form["has-read-book"].value
+    );
+    updateDisplay();
+  },
+  false
+);
+function displayBooks() {
+  for (book of myLibrary) {
+    const newRow = table.insertRow();
+    for (data of Object.values(book)) {
+      if (typeof data === "function") {
+        continue;
+      }
+      const newCell = newRow.insertCell();
+      newCell.textContent = data;
+    }
+  }
+}
+displayBooks();
+function updateDisplay() {
   const newRow = table.insertRow();
-  for (data of Object.values(book)) {
+  for (data of Object.values(myLibrary[myLibrary.length - 1])) {
     if (typeof data === "function") {
       continue;
     }
